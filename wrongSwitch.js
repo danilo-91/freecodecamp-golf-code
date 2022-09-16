828d3c07f78ecf899b75cf985b1c7d1478529566
Daniel Ise <ise.daniel@gmail.com>
2022-09-16 19:05:41 -0300

    testing switch bad uses

---
+++ wrongSwitch.js
@@ -0,0 +1,25 @@
+const wrongSwitch = (value) => {
+  let answer = "";
+  switch(value) {
+    case 1:
+      answer += "one";
+    case 2:
+      answer += "two";
+      break;
+    case 3:
+      answer += "three";
+      break;
+    case 4:
+      answer += "four";
+    case 5:
+      answer += "five";
+  }
+  return answer;
+}
+
+console.log(wrongSwitch(1));
+console.log(wrongSwitch(2));
+console.log(wrongSwitch(3));
+console.log(wrongSwitch(4));
+console.log(wrongSwitch(5));
+
