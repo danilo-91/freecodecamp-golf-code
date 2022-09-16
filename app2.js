47f452a519f12a6a7924cc4f5a5e6ec208edfc5b
Daniel Ise <ise.daniel@gmail.com>
2022-09-16 18:48:04 -0300

    some minor fixes

--- app2.js
+++ app2.js
@@ -2,12 +2,12 @@ const names = ["Hole-in-one!", "Eagle", "Birdie", "Par", "Bogey", "Double Bogey"
 
 function golfScore(par, strokes) {
   // Only change code below this line
-  if (strokes === 1) return names[0];
+  if (strokes === 1)	  return names[0];
   if (strokes <= par - 2) return names[1];
-  if (strokes <= par - 1) return names[2];
-  if (strokes <= par) return names[3];
-  if (strokes <= par + 1) return names[4];
-  if (strokes <= par + 2) return names[5];
+  if (strokes == par - 1) return names[2];
+  if (strokes == par)	  return names[3];
+  if (strokes == par + 1) return names[4];
+  if (strokes == par + 2) return names[5];
   return names[6];
   // Only change code above this line
 }
